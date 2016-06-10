37d55fa6efadeac4a30163f6af6cbefa01f63b06
cory <whippoorwill.in.flight@gmail.com>
2016-06-10 09:25:41 -0700

    add question-tile

---
+++ app/models/question.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+
+export default DS.Model.extend({
+  corpse: DS.attr(),
+  author: DS.attr(),
+  notes: DS.attr()
+});
---
+++ app/routes/index.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+var questions = [{
+  id: 1,
+  corpse: "What is the color of the moon?",
+  author: "Chairface",
+  notes: "no no notes"
+}, {
+  id: 2,
+  corpse: "How did a tree get its name?",
+  author: "Sofrates",
+  notes: "noted"
+}, {
+  id: 3,
+  corpse: "Which is better, Ember or ice cream?",
+  author: "Nancy",
+  notes: "feeling troubled"
+}]
+
+export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('question');
+  },
+});
